d49389feaf3f6c6cda9b2e68111897150a2e1f43
SofVer10 <20230106@ricaldone.edu.sv>
2025-06-09 11:16:29 -0600

    documentación agregada

--- backend/app.js
+++ backend/app.js
@@ -4,29 +4,40 @@ import express from "express";
 
 //PASO 9 
 // esta linea es para cuando ya se hayan credo los metodos
-import productsRoutes from "./src/routes/products.js"
-import clientsRoutes from "./src/routes/clients.js"
-import employeesRoutes from "./src/routes/employees.js"
-import localsRoutes from "./src/routes/locals.js"
-import categoriesRoutes from "./src/routes/categories.js"
-import reviewsRoutes from "./src/routes/reviews.js"
-import evaluationRoutes from "./src/routes/evaluations.js"
-import registerEmployeesRoutes from "./src/routes/registerEmployees.js"
-import loginRoutes from "./src/routes/login.js"
+import productsRoutes from "./src/routes/products.js";
+import clientsRoutes from "./src/routes/clients.js";
+import employeesRoutes from "./src/routes/employees.js";
+import localsRoutes from "./src/routes/locals.js";
+import categoriesRoutes from "./src/routes/categories.js";
+import reviewsRoutes from "./src/routes/reviews.js";
+import evaluationRoutes from "./src/routes/evaluations.js";
+import registerEmployeesRoutes from "./src/routes/registerEmployees.js";
+import loginRoutes from "./src/routes/login.js";
 //Importar libreria para que PostMan acepte cookies
 import cookieParser from "cookie-parser";
-import logoutRoutes from "./src/routes/logout.js"
-import registerClients from "./src/routes/registerClients.js"
-import passwordRecoveryRoutes from "./src/routes/passwordRecovery.js"
-import blogRoute from "./src/routes/blog.js"
+import logoutRoutes from "./src/routes/logout.js";
+import registerClients from "./src/routes/registerClients.js";
+import passwordRecoveryRoutes from "./src/routes/passwordRecovery.js";
+import blogRoute from "./src/routes/blog.js";
 
 import { validateAuthToken } from "./src/middlewares/validateAuthToken.js";
+
+//Importar libreria para Swagger
+import swaggerUI from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
+
 
 //PASO 1 (Continuación)
 //Creo una constante que es igual
 //a la libreria que importe y la ejecuta
 const app = express();
 
+//Utilizar el sistema de archivos para leer ek JSON  de Swagger y ver mi documentación
+
+const swaggerDocument = JSON.parse(fs.readFileSync(
+    path.resolve("./ricaldone-d95-PepsiMERN-1.0.0-resolved.json"), "utf-8"
+))
 
 
 //PASO 10
@@ -53,6 +64,9 @@ app.use("/api/logout", logoutRoutes);
 app.use("/api/registerClients", registerClients);
 app.use("/api/passwordRecoveryRoutes", passwordRecoveryRoutes);
 app.use("/api/blog", blogRoute);
+
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
 
 //PASO 1 (Continuación)
 //exporta la constante para poder usar express en otros lados
--- frontend/pepsi/src/pages/Home.jsx
+++ frontend/pepsi/src/pages/Home.jsx
@@ -21,21 +21,12 @@ const Home = () => {
               <div className="bar"></div>
             </div>
           </div>
-          <span>Journey to new frontiers. Journey to Pepsi Experience</span>
+
         </div>
         
         <h1 className="main-title">PEPSI</h1>
         
-        <p className="description">
-          Away from the mundane energy of today's famous metropoles, lies the 
-          ancient land of spirits. Swimming and captivating in equal measures, 
-          step into a different weather. Sweeping peaks, lakes and ancient waterfalls.
-        </p>
-        
-        <button className="cta-button">
-          Start the journey
-          <span className="arrow">→</span>
-        </button>
+      
       </div>
     </div>
   );
